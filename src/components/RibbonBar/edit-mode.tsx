d4a9c695dc95fed413e39b9cc704ee5b818f4d0f
Your Name <you@example.com>
2026-04-10 02:29:26 -0400

    move preview panel to Time mode

--- src/components/RibbonBar/edit-mode.tsx
+++ src/components/RibbonBar/edit-mode.tsx
@@ -47,7 +47,6 @@ import {
 	selectedLinesAtom,
 	selectedWordsAtom,
 	showEndTimeAsDurationAtom,
-	showPreviewPanelAtom,
 } from "$/states/main.ts";
 import { type LyricLine, type LyricWord, newLyricLine } from "$/types/ttml";
 import { msToTimestamp, parseTimespan } from "$/utils/timestamp.ts";
@@ -716,8 +715,6 @@ export const EditModeRibbonBar: FC = forwardRef<HTMLDivElement>(
 	(_props, ref) => {
 		const editLyricLines = useSetImmerAtom(lyricLinesAtom);
 		const { t } = useTranslation();
-		const [showPreviewPanel, setShowPreviewPanel] =
-			useAtom(showPreviewPanelAtom);
 
 		return (
 			<RibbonFrame ref={ref}>
@@ -864,14 +861,6 @@ export const EditModeRibbonBar: FC = forwardRef<HTMLDivElement>(
 					label={t("ribbonBar.editMode.auxiliaryLineDisplay", "辅助行显示")}
 				>
 					<AuxiliaryDisplayField />
-				</RibbonSection>
-				<RibbonSection label={t("ribbonBar.editMode.previewPanel", "预览面板")}>
-					<Flex direction="column" align="center" gap="1">
-						<Switch
-							checked={showPreviewPanel}
-							onCheckedChange={setShowPreviewPanel}
-						/>
-					</Flex>
 				</RibbonSection>
 			</RibbonFrame>
 		);
